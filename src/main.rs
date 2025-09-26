6d2481473f968373cd9a05f545579e7faf22a823
onefriendlycoder <abhyanand311099@gmail.com>
2025-09-26 20:21:41 +0530

    basic layout complete

--- src/main.rs
+++ src/main.rs
@@ -4,80 +4,71 @@ use macroquad::prelude::*;
 async fn main() {
     set_fullscreen(true);
 
-    set_fullscreen(true);
-    let screen_height:f32 = screen_height();
-    let screen_width:f32 = screen_width();
-    let screen_depth:f32 = screen_height.max(screen_width);
+    let screen_h: f32 = screen_height();
+    let screen_w: f32 = screen_width();
+    let screen_d: f32 = screen_h.max(screen_w);
+    let road_half = 0.01;
+    let lane_half = 0.005;
+    let grass_half = 0.09;
+    let red_half = 0.32;
+
     let camera = Camera3D {
-        position: vec3(0.0, 5.0, 0.0),
-        target: vec3(0.0, 0.0, screen_depth),
+        position: vec3(0.0, screen_h*0.01, 0.0),
+        target: vec3(0.0, 0.0, screen_d),
         up: vec3(0.0, 1.0, 0.0),
         ..Default::default()
     };
+
     loop {
         clear_background(BLUE);
         set_camera(&camera);
+
         draw_cube(
-            vec3(0.0, 0.0, screen_depth),
-            vec3(1.0, 1.0, 4.0),
+            vec3(0.0, -1.0, 0.0),
+            vec3(screen_w * road_half * 2.0, 0.1, screen_d),
+            None,
+            DARKGRAY,
+        );
+
+        draw_cube(
+            vec3(-screen_w * (road_half + lane_half / 2.0), -1.0, 0.0),
+            vec3(screen_w * lane_half, 0.1, screen_d),
+            None,
+            BLACK,
+        );
+        draw_cube(
+            vec3(screen_w * (road_half + lane_half / 2.0), -1.0, 0.0),
+            vec3(screen_w * lane_half, 0.1, screen_d),
+            None,
+            BLACK,
+        );
+
+        draw_cube(
+            vec3(-screen_w * (road_half + lane_half + grass_half / 2.0), -1.0, 0.0),
+            vec3(screen_w * grass_half, 0.1, screen_d),
+            None,
+            GREEN,
+        );
+        draw_cube(
+            vec3(screen_w * (road_half + lane_half + grass_half / 2.0), -1.0, 0.0),
+            vec3(screen_w * grass_half, 0.1, screen_d),
+            None,
+            GREEN,
+        );
+
+        draw_cube(
+            vec3(-screen_w * (road_half + lane_half + grass_half + red_half / 2.0), -1.0, 0.0),
+            vec3(screen_w * red_half, 0.1, screen_d),
+            None,
+            RED,
+        );
+        draw_cube(
+            vec3(screen_w * (road_half + lane_half + grass_half + red_half / 2.0), -1.0, 0.0),
+            vec3(screen_w * red_half, 0.1, screen_d),
             None,
             RED,
         );
 
-        draw_cube(
-            vec3(0.0, -1.0, 0.0),
-            vec3(12.0, 0.1, screen_depth),
-            None,
-            DARKGRAY,
-        );
-        draw_cube(
-            vec3(-8.0, -1.0, 0.0),
-            vec3(4.0, 0.1, screen_depth),
-            None,
-            BLACK,
-        );
-        draw_cube(
-            vec3(-30.0, -1.0, 0.0),
-            vec3(40.0, 0.1, screen_depth),
-            None,
-            GREEN,
-        );
-        draw_cube(
-            vec3(8.0, -1.0, 0.0),
-            vec3(4.0, 0.1, screen_depth),
-            None,
-            BLACK,
-        );
-        draw_cube(
-            vec3(30.0, -1.0, 0.0),
-            vec3(40.0, 0.1, screen_depth),
-            None,
-            GREEN,
-        );
-        draw_cube(
-            vec3(-70.0, -1.0, 0.0),
-            vec3(40.0, 0.1, screen_depth),
-            None,
-            RED,
-        );
-        draw_cube(
-            vec3(70.0, -1.0, 0.0),
-            vec3(40.0, 0.1, screen_depth),
-            None,
-            RED,
-        );
-        draw_cube(
-            vec3(-75.0, -1.0, 0.0),
-            vec3(50.0, 0.1, screen_depth),
-            None,
-            RED,
-        );
-        draw_cube(
-            vec3(75.0, -1.0, 0.0),
-            vec3(50.0, 0.1, screen_depth),
-            None,
-            RED,
-        );
         next_frame().await;
     }
 }
